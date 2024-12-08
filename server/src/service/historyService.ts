6c393fb198c1c2f590ed5ff40713e5be9e381ad1
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-12-08 14:36:27 -0600

    updated with proper functionality

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,79 +1,70 @@
-// import fs from 'node:fs/promises';
-// import { v4 as uuidv4 } from 'uuid';
+import fs from 'node:fs/promises';
+import { v4 as uuidv4 } from 'uuid';
 
-// // TODO: Define a City class with name and id properties
+// TODO: Define a City class with name and id properties
 
-// class City {
-//   name: string;
-//   id: string;
+class City {
+    name: string;
+    id: string;
 
-//   constructor(name: string, id: string) {
-//     this.name = name;
-//     this.id = id;
-//   }
-// }
+    constructor(name: string, id: string) {
+        this.name = name;
+        this.id = id;
+    }
+}
 
-// // TODO: Complete the HistoryService class
-// class HistoryService {
-  
-//   // TODO: Define a read method that reads from the searchHistory.json file
-//   private async read() {
-//     return await fs.readFile('db/db.json', {
-//       flag: 'a+',
-//       encoding: 'utf8',
-//     });
-//   }
+// TODO: Complete the HistoryService class
+class HistoryService {
 
-//   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-//   // private async write(cities: City[]) {}
-//   private async write(cities: City[]) {
-//     return await fs.writeFile('db/db.json', JSON.stringify(cities, null, '\t'));
-//   }
+    // TODO: Define a read method that reads from the searchHistory.json file
+    private async read() {
+        return await fs.readFile('db/db.json', {
+            flag: 'a+',
+            encoding: 'utf8',
+        });
+    }
 
-//   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-//   // async getCities() {}
-//   async getCities() {
-//     return await this.read().then((cities) => {
-//       let parsedCities: City[];
+    // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
+    private async write(cities: City[]) {
+        return await fs.writeFile('db/db.json', JSON.stringify(cities, null, '\t'));
+    }
 
-//       // If cities isn't an array or can't be turned into one, send back a new empty array
-//       try {
-//         parsedCities = [].concat(JSON.parse(cities));
-//       } catch (err) {
-//         parsedCities = [];
-//       }
+    // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
+    async getCities() {
+        return await this.read().then((cities) => {
+            let parsedCities: City[];
 
-//       return parsedCities;
-//     });
-//   }
-// // TODO Define an addCity method that adds a city to the searchHistory.json file
-//   // async addCity(city: string) {}
-//   async addCity(city: string) {
-//     if (!city) {
-//       throw new Error('state cannot be blank');
-//     }
+            // If cities isn't an array or can't be turned into one, send back a new empty array
+            try {
+                parsedCities = [].concat(JSON.parse(cities));
+            } catch (err) {
+                parsedCities = [];
+            }
 
-//     // Add a unique id to the state using uuid package
-//     const newCity: City = { name: city, id: uuidv4() };
+            return parsedCities;
+        });
+    }
 
-//     // Get all cities, add the new city, write all the updated cities, return the newCity
-//     return await this.getCities()
-//       .then((cities) => {
-//         if (cities.find((index) => index.name === city)) {
-//           return cities;
-//         }
-//         return [...cities, newCity];
-//       })
-//       .then((updatedCities) => this.write(updatedCities))
-//       .then(() => newCities);
-//   }
-// // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-//   // async removeCity(id: string) {}
-//   async removeCity(id: string) {
-//     return await this.getCities()
-//       .then((cities) => cities.filter((city) => city.id !== id))
-//       .then((filteredCities) => this.write(filteredCities));
-//   }
-// }
+    // TODO Define an addCity method that adds a city to the searchHistory.json file
+    async addCity(city: string) {
+        if (!city) {
+            throw new Error('city cannot be blank');
+        }
 
-// export default new HistoryService();
+        // Add a unique id to the state using uuid package
+        const newCity: City = { name: city, id: uuidv4() };
+
+        // Get all cities, add the new city, write all the updated cities, return the newCity
+        return await this.getCities()
+            .then((cities) => {
+                if (cities.find((index) => index.name === city)) {
+                    return cities;
+                }
+                return [...cities, newCity];
+            })
+            .then((updatedCities) => this.write(updatedCities))
+            .then(() => newCity);
+    }
+}
+
+export default new HistoryService();
--- server/src/service/weatherService.ts
+++ server/src/service/weatherService.ts
@@ -1,188 +1,166 @@
-// import fs from 'node:fs/promises'; // grabbed from parks
 import dotenv from 'dotenv';
 dotenv.config();
+import dayjs, { type Dayjs } from 'dayjs';
 
-// // TODO: Define an interface for the Coordinates object
-// interface Park {
-//   id: string;
-//   fullName: string;
-//   description: string;
-//   url: string;
-//   designation: string;
-//   images: ParkImage[];
-// }
-
-// interface ParkImage {
-//   url: string;
-//   title: string;
-//   altText: string;
-// }
-
-// interface ParkEvent {
-//   id: string;
-//   title: string;
-//   location: string;
-//   description: string;
-//   infourl: string;
-//   datestart: string;
-//   dateend: string;
-// }
-
-// interface StateObject {
-//   stateName: string;
-//   stateCode: string;
-// }
+// TODO: Define an interface for the Coordinates object
+interface Coordinates {
+  name: string;
+  lat: number;
+  lon: number;
+  country: string;
+  state: string;
+}
 
 // TODO: Define a class for the Weather object
-// class Weather {
-  // name: string;
-  // id: string;
-  
-
-  // constructor(name: string, id: string) {
-  //   this.name = name;
-  //   this.id = id;
-  // }
-// }
+class Weather {
+  city: string;
+  date: Dayjs | string;
+  tempF: number;
+  windSpeed: number;
+  humidity: number;
+  icon: string;
+  iconDescription: string;
+  constructor(city: string, date: Dayjs | string, tempF: number, windSpeed: number, humidity: number, icon: string, iconDescription: string) {
+    this.city = city,
+      this.date = date,
+      this.tempF = tempF,
+      this.windSpeed = windSpeed,
+      this.humidity = humidity,
+      this.icon = icon,
+      this.iconDescription = iconDescription
+  }
+}
 
 
 // TODO: Complete the WeatherService class
 class WeatherService {
   // // TODO: Define the baseURL, API key, and city name properties
+  private baseURL?: string;
+  private apiKey?: string;
+  private city = '';
 
-  // private baseURL?: string;
+  constructor() {
+    this.baseURL = process.env.API_BASE_URL || '';
+    this.apiKey = process.env.API_KEY || '';
+  }
 
-  // private apiKey?: string;
+  // TODO: Create fetchLocationData method
+  private async fetchLocationData(query: string) {
+    try {
+      if (!this.baseURL || !this.apiKey) {
+        throw new Error('no base URL and/or API key found!');
+      }
+      const response: Coordinates[] = await fetch(query).then((res) => res.json());
+      return response[0];
 
-  // constructor() {
-  //   this.baseURL = process.env.API_BASE_URL || '';
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  }
 
-  //   this.apiKey = process.env.API_KEY || '';
-  // }
-  // // TODO: Create fetchLocationData method
-  // private async fetchLocationData(query: string) {
+  // TODO: Create destructureLocationData method
+  private destructureLocationData(locationData: Coordinates): Coordinates {
+    if (!locationData) {
+      throw new Error('could not find city!');
+    }
+    const { name, lat, lon, country, state } = locationData;
 
-  // }
-  // // TODO: Create destructureLocationData method
-  // // private destructureLocationData(locationData: Coordinates): Coordinates {}
-  // // TODO: Create buildGeocodeQuery method
-  // // private buildGeocodeQuery(): string {}
-  // // TODO: Create buildWeatherQuery method
-  // // private buildWeatherQuery(coordinates: Coordinates): string {}
-  // // TODO: Create fetchAndDestructureLocationData method
-  // // private async fetchAndDestructureLocationData() {}
-  // // TODO: Create fetchWeatherData method
-  // // private async fetchWeatherData(coordinates: Coordinates) {}
-  // // TODO: Build parseCurrentWeather method
-  // // private parseCurrentWeather(response: any) {}
-  // // TODO: Complete buildForecastArray method
-  // // private buildForecastArray(currentWeather: Weather, weatherData: any[]) {}
-  // // TODO: Complete getWeatherForCity method
-  // // async getWeatherForCity(city: string) {}
+    const coordinates: Coordinates = { name, lat, lon, country, state };
 
-  // async getParksByState(state: string) {
-  //   try {
-  //     const response = await fetch(
-  //       `${this.baseURL}/parks?limit=10&stateCode=${state}&api_key=${this.apiKey}`
-  //     );
+    return coordinates;
+  }
 
-  //     const parks = await response.json();
+  // TODO: Create buildGeocodeQuery method
+  private buildGeocodeQuery(): string {
+    const geocodeQuery = `${this.baseURL}/geo/1.0/direct?q=${this.city}&limit=1&appid=${this.apiKey}`;
+    return geocodeQuery;
+  }
 
-  //     const mappedParks = await this.parkDataMapping(parks.data);
-  //     return mappedParks;
-  //   } catch (err) {
-  //     console.log('Error:', err);
-  //     return err;
-  //   }
-  // }
+  // TODO: Create buildWeatherQuery method
+  private buildWeatherQuery(coordinates: Coordinates): string {
+    const weatherQuery = `${this.baseURL}/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&units=imperial&appid=${this.apiKey}`;
+    return weatherQuery;
+  }
 
-  // async parkDataMapping(parks: Park[]) {
-  //   const parksArray: Park[] = parks.map((park) => {
-  //     const parkObject: Park = {
-  //       id: park.id,
-  //       fullName: park.fullName,
-  //       description: park.description,
-  //       url: park.url,
-  //       designation: park.designation,
-  //       images: park.images,
-  //     };
+  // TODO: Create fetchAndDestructureLocationData method
+  private async fetchAndDestructureLocationData() {
+    return await this.fetchLocationData(this.buildGeocodeQuery()).then((data) =>
+      this.destructureLocationData(data));
+  }
 
-  //     return parkObject;
-  //   });
+  // TODO: Create fetchWeatherData method
+  private async fetchWeatherData(coordinates: Coordinates) {
+    try {
+      const response = await fetch(this.buildWeatherQuery(coordinates))
+        .then(
+          (res) => res.json()
+        );
+      if (!response) {
+        throw new Error('cannot find weather data');
+      }
 
-  //   return parksArray;
-  // }
+      const currentWeather: Weather = this.parseCurrentWeather(
+        response.list[0]
+      );
 
-  // async getClosestEventByState(state: string): Promise<ParkEvent | string> {
-  //   try {
-  //     const response = await fetch(
-  //       `${this.baseURL}/events?limit=10&stateCode=${state}&api_key=${this.apiKey}`
-  //     );
+      const forecast: Weather[] = this.buildForecastArray(
+        currentWeather, response.list
+      );
 
-  //     const events = await response.json();
+      return forecast;
 
-  //     const mappedEvents = await this.parkEventDataMapping(events.data);
+    } catch (err: any) {
+      console.error(err);
+      return err;
+    }
+  }
 
-  //     if (mappedEvents.length === 0) {
-  //       return 'No events found';
-  //     }
+  // TODO: Build parseCurrentWeather method
+  private parseCurrentWeather(response: any) {
+    const parsedDate = dayjs.unix(response.dt).format('M/D/YYYY');
 
-  //     const closestEvent = await this.findClosestParkEvent(mappedEvents);
-  //     return closestEvent;
-  //   } catch (err: any) {
-  //     throw new Error(err);
-  //   }
-  // }
+    const currentWeather = new Weather(this.city, parsedDate, response.main.temp, response.wind.speed, response.main.humidity, response.weather[0].icon, response.weather[0].description || response.weather[0].main);
 
-  // async parkEventDataMapping(parkEvent: ParkEvent[]) {
-  //   const parkEventsArray: ParkEvent[] = parkEvent.map((occasion) => {
-  //     const parkEventObject: ParkEvent = {
-  //       id: occasion.id,
-  //       title: occasion.title,
-  //       location: occasion.location,
-  //       description: occasion.description,
-  //       infourl: occasion.infourl || 'No URL Available',
-  //       datestart: occasion.datestart,
-  //       dateend: occasion.dateend,
-  //     };
+    return currentWeather;
+  }
 
-  //     return parkEventObject;
-  //   });
+  // TODO: Complete buildForecastArray method
+  private buildForecastArray(currentWeather: Weather, weatherData: any[]) {
+    const weatherForecast: Weather[] = [currentWeather];
 
-  //   return parkEventsArray;
-  // }
+    const filteredWeatherData = weatherData.filter((data: any) => {
+      return data.dt_txt.includes('12:00:00');
+    });
 
-  // async findClosestParkEvent(events: ParkEvent[]) {
-  //   const today = new Date();
-  //   const closestEvent = events.reduce((prev, current) => {
-  //     const prevDate = new Date(prev.datestart);
-  //     const currentDate = new Date(current.datestart);
-  //     const prevDiff = Math.abs(today.getTime() - prevDate.getTime());
-  //     const currentDiff = Math.abs(today.getTime() - currentDate.getTime());
-  //     return prevDiff < currentDiff ? prev : current;
-  //   });
+    for (const day of filteredWeatherData) {
+      weatherForecast.push(
+        new Weather(this.city, dayjs.unix(day.dt).format('M/D/YYYY'), day.main.temp, day.wind.speed, day.main.humidity, day.weather[0].icon, day.weather[0].description || day.weather[0].main));
+    }
 
-  //   return closestEvent;
-  // }
+    return weatherForecast;
+  }
 
-  // async convertStateCodeToName(stateCode: string) {
-  //   const stateCodes = await this.read();
-  //   const parsedStateCodes = JSON.parse(stateCodes);
-  //   const foundState = parsedStateCodes.filter((stateObject: StateObject) => {
-  //     //ensuring casing of input does not matter
-  //     return stateObject.stateCode.toLowerCase() === stateCode.toLowerCase();
-  //   });
+  // TODO: Complete getWeatherForCity method
+  async getWeatherForCity(city: string) {
+    try {
+      this.city = city;
+      
+      const coordinates = await this.fetchAndDestructureLocationData();
 
-  //   const stateName = foundState[0].stateName;
-  //   return stateName;
-  // }
+      if (coordinates) {
+        const weather = await this.fetchWeatherData(coordinates);
+        return weather;
+      } else if (!coordinates) {
+        throw new Error('could not find weather data');
+      }
+      
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
 
-  // private async read() {
-  //   return await fs.readFile('db/stateCodes.json', {
-  //     flag: 'a+',
-  //     encoding: 'utf8',
-  //   });
-  // }
 }
 
 export default new WeatherService();
