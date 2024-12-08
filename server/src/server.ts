9f3b3bc561e32d087f655c5158d30431c06ab363
dcartolano <180475468+dcartolano@users.noreply.github.com>
2024-12-08 15:09:33 -0600

    cleaned up

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,14 +11,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
-app.use(express.static('../client/dist')); // copied from miniproject
+app.use(express.static('../client/dist'));
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
-app.use(express.urlencoded({ extended: true })); // copied from miniproject
-app.use(express.json()); // copied from miniproject
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // TODO: Implement middleware to connect the routes
-app.use(routes); // copied from miniproject
+app.use(routes);
 
 // Start the server on the port
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
